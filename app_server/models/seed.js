271e986619c436b704f4e908af4402ffd2956b7c
Matthew Bridegroom <matthew.bridegroom@snhu.edu>
2026-06-07 20:42:17 -0500

    Module 4 Completed Baseline

---
+++ app_server/models/seed.js
@@ -0,0 +1,20 @@
+// Bring in the DB conneciton and the Trip Schema
+const Mongoose = require('./db');
+const Trip = require('./travlr');
+
+// Read seed data from json file
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+
+// delete any existing recors, then insert seed data
+const seedDB = async () => {
+    await Trip.deleteMany({});
+    await Trip.insertMany(trips);
+};
+
+// Close the MongoDB conneciton and exit
+seedDB().then(async () => {
+    await Mongoose.connection.close();
+    process.exit(0);
+});
